bbb8d21c2b6201bf1d53a12d12e95a8f13928a73
Durgesh kumar <durgeshk@snakconsultancy.com>
2023-11-14 17:17:34 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Swiper from 'react-id-swiper';
 
 function App() {
+  const params = {
+    lazy: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Swiper {...params}>
+      <div>
+        <img
+          alt="img"
+          data-src="https://d1820pa95bipcd.cloudfront.net/files/a011dcc4777e4c88b074ded8d85934c0/content?width=426"
+          className="swiper-lazy"
+        />
+        <div className="swiper-lazy-preloader swiper-lazy-preloader-white" />
+      </div>
+      <div>
+        <img
+          alt="img"
+          data-src="https://d1820pa95bipcd.cloudfront.net/files/c2684d5adcb34af2a4299f4a88cb3b59/content?width=426"
+          className="swiper-lazy"
+        />
+        <div className="swiper-lazy-preloader swiper-lazy-preloader-white" />
+      </div>
+      <div>
+        <img
+          alt="img"
+          data-src="https://d1820pa95bipcd.cloudfront.net/files/44ae66fc97db467c8a195b7e3963e5a1/content?width=426"
+          className="swiper-lazy"
+        />
+        <div className="swiper-lazy-preloader swiper-lazy-preloader-white" />
+      </div>
+      <div>
+        <img
+          alt="img"
+          src="https://d1820pa95bipcd.cloudfront.net/files/c50b3e44a5634874bfdec41cce412fdc/content?width=426"
+          className="swiper-lazy"
+        />
+        <div className="swiper-lazy-preloader swiper-lazy-preloader-white" />
+      </div>
+    </Swiper>
+  </div>
   );
 }
 
